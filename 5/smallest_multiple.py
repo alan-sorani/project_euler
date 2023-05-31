c20ce58a31c65f89b07ab623db5733d843299858
eladtzorani <tzorani.elad@gmail.com>
2023-05-31 13:48:15 +0300

    solved 5 and corrected prime check on 3

--- 5/smallest_multiple.py
+++ 5/smallest_multiple.py
@@ -1,4 +1,4 @@
-from math import floor, sqrt
+from math import floor, sqrt, log
 
 """
 
@@ -19,14 +19,15 @@ def is_prime(n):
     return result
 
 """
-largest_prime_factor
+smallest_multiple
 
 Input: an integer n
-Output: the largest prime factor of n
+Output: the smallest multiple of all positive integers up to n
 """
 
-def largest_prime_factor(n):
-    k = floor(sqrt(n))
-    while(n/k != int(n/k) or is_prime(k) == False):
-        k -= 1
-    return k
+def smallest_multiple(n):
+    result = 1
+    for i in range(2,n+1):
+        if is_prime(i):
+            result*=i**(floor(log(n,i)))
+    return result
