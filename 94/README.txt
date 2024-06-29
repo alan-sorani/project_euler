f5d924404777558235b8dc6d7fe2712addac4cb8
Alan Sorani <al.sorani@proton.me>
2024-06-30 01:29:56 +0300

    added solution for 94

---
+++ 94/README.txt
@@ -0,0 +1,3 @@
+It seems that the result from the code has rounding errors. Summing the values in lines 1-13,15 from wrong_result.txt gives the correct result.
+
+I'll fix this in the future.
---
+++ 94/almost_equilateral_triangles.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+'''
+The area of a triangle with sides a,a,b is :math:`\sqrt{a^2b^2 - \frac{b^2}{4}}`, as is computed by placing the length b orthogonal to the x-axis with the vertex in front of it at (0,0), and using trigonometry.
+For this value to be an integer n, we get that :math:`16 n^2 = 4 a^2 b^2 - b^4`, so that b has to be of the form 2m for m an integer. Substituting b=2m, we get that the area is integral if and only if :math:`m^2(a^2 - m^2) = n^2` is a square integer. Since :math:`m^2, (a^2 - m^2)` are both integers, and since :math:`m^2` is a square, this is equivalent to :math:`a^2 - m^2 = a^2 - \left(\frac{b}{2}\right)^2` being a square.
+'''
+
+max_perimeter = 1_000_000_000
+a = 2
+res = 0
+while((a-1)*3 <= max_perimeter):
+    for b in {a-1, a+1}:
+        test = np.sqrt(a**2 - (b/2)**2) 
+        if(test == int(np.rint(test))):
+            print(f"({a},{a},{b}): {np.sqrt(a**2 * b**2 / 4 - b**4/16)}")
+            res += 2*a + b
+    a += 1
+print(res)
