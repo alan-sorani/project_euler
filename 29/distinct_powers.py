b2e5b5bf0d7d8ff05b0e2ecafcf5c8790e152457
Alan Sorani <al.sorani@proton.me>
2024-06-29 17:41:35 +0300

    added solution for 29

--- 29/distinct_powers.py
+++ 29/distinct_powers.py
@@ -1,1 +1,14 @@
 import numpy as np
+
+'''
+The problem is easy enough that we can brute-force through it.
+'''
+
+max_num = 100
+res = []
+
+for a in range(2,max_num+1):
+    for b in range(2,max_num+1):
+        res += [a**b]
+
+print(len(set(res)))
