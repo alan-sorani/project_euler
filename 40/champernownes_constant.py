557a40e55d99aa4c8baf1a59297ac94a5bddb3cf
Alan Sorani <al.sorani@proton.me>
2024-07-02 10:18:03 +0300

    added solution 40

--- 40/champernownes_constant.py
+++ 40/champernownes_constant.py
@@ -7,18 +7,24 @@ To find the n'th digit, we first find how many digits the number its contained i
 '''
 
 num_digits_in_length = [9 * 10**(k-1) * k for k in range(20)]
-num_digits_up_to_length = [np.sum(num_digits_in_length[:k]) for k in range(20)]
+num_digits_up_to_length = [int(np.sum(num_digits_in_length[:k])) for k in range(20)]
 
 def find_digit(digit_index : int):
     length_of_num = 0
     for i, num_digits in enumerate(num_digits_up_to_length):
-        if (num_digits > digit_index):
+        if (num_digits >= digit_index):
             length_of_num = i - 1
-            num_in_length = np.floor((digit_index - num_digits) / i)
             break
-    num_of_digit = num_in_length + 10**(length_of_num - 1)
-    return num_of_digit
+    # the index of the digit to be searched when starting to count only from numbers of the specific length
+    digit_in_length = digit_index - num_digits_up_to_length[length_of_num]
+    # the index of the number within numbers of that length, in which the digit appears
+    relative_num_of_digit = int(np.ceil(digit_in_length / length_of_num))
+    num_of_digit = relative_num_of_digit + 10**(length_of_num - 1) - 1
+    index_in_num = digit_in_length - length_of_num * (relative_num_of_digit - 1)
+    return int(str(num_of_digit)[index_in_num-1])
 
 if(__name__ == "__main__"):
-    print(find_digit(100))
-    
+    res = 1
+    for i in range(7):
+        res *= find_digit(10**i)
+    print(res)
