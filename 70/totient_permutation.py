a3a206dbe302f627b2100194fd9cd1e9b8a502ea
Alan Sorani <al.sorani@proton.me>
2024-10-05 09:07:35 +0300

    removed unused functions in solution 69

--- 70/totient_permutation.py
+++ 70/totient_permutation.py
@@ -11,20 +11,11 @@ from math import log
 '''
 From the formula :math:`\phi(n) = n \prod_{p \mid n} \left(1 - \frac{1}{p}\right)` we get that :math:`\frac{n}{\phi(n)} = \left(\prod_{p \mid n} \left(1 - \frac{1}{p} \right) \right)^{-1}`.
 
-This is maximal when the product :math:\prod_{p \mid n} \left(1 - \frac{1}{p}\right)` is minimal. Taking the logarithm of this, we have to minimize :math:`\sum_{p \mid n} \log\left(1 - \frac{1}{p}\right)`.
+This is minimal when the product :math:\prod_{p \mid n} \left(1 - \frac{1}{p}\right)` is maximal. Taking the logarithm of this, we have to maximize :math:`\sum_{p \mid n} \log\left(1 - \frac{1}{p}\right)`.
 '''
 
 def solution():
-    primes = sieve_of_eratosthenes(10**6)
-    i = 1
-    min_prod = 2
-    res = None
-    while(min_prod <= 10**6):
-        res = min_prod
-        i += 1
-        min_prod = np.prod(primes[:i])
-    return res
-
+    primes = sieve_of_eratosthenes(10**7)
 
 if __name__ == "__main__":
     print(solution())
